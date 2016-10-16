f785ea2509f43834a1e3360fe42f7b1b45a41acb
Gustavo López Blandon <gustavo@kommit.co>
2016-10-16 16:57:31 -0500

    App Layout + Component Setup

---
+++ client/reduxstagram.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { render } from 'react-dom'
+
+// Import css
+import css from './styles/style.styl'
+
+// Import Components
+import Main from './components/Main'
+
+render(<Main />, document.getElementById('root'))
