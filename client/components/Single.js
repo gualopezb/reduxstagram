8431fa64a6fecb9b73b7e9ad6b05426668d3e030
Gustavo López Blandon <gustavo@kommit.co>
2016-10-17 22:44:49 -0500

    Displaying the Single Photo Component

--- client/components/Single.js
+++ client/components/Single.js
@@ -1,10 +1,20 @@
 import React from 'react'
+import Photo from './Photo'
+import Comments from './Comments'
 
 const Single = React.createClass({
   render() {
+    // index of the post
+    const i = this.props.posts.findIndex((post) => post.code === this.props.params.postId)
+    const post = this.props.posts[i]
+    
     return (
-      <div className="photo-grid">
-        I'm the Single
+      <div className="single-photo">
+        <Photo 
+          i={i}
+          post={post}
+          {...this.props} />
+        <Comments />
       </div>
     )
   }
